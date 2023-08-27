a0a12c1208eb6c1f37821d355b00121a8a8ecfb4
Alperen ERYILMAZ <mchtter@gmail.com>
2023-08-27 14:22:54 +0300

    client[develop]: loading spinner component name specified

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -1,4 +1,4 @@
-export function Loading() {
+export function LoadingSpinner() {
     return (
         <div className="flex items-center justify-center h-screen">
             <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-950 dark:border-gray-100 z-40"></div>
