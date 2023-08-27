6d98a74d21efb00a7dbad2911ef6a42d7c1982e9
Alperen ERYILMAZ <mchtter@gmail.com>
2023-08-27 18:03:23 +0300

    server[develop]: mock data updated and API data handle is done

--- src/mocks/flights.mock.js
+++ src/mocks/flights.mock.js
@@ -1,28 +1,264 @@
 export const flights = [
     {
         "id": 1,
-        "from": "Istanbul(SAW)",
-        "to": "Adana(ADA)",
-        "departure": "2021-10-10",
-        "passenger": 1,
-        "price": 200
+        "airline": "Turkish Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "10:00",
+        "remainingSeat": 7,
+        "price": "200"
     },
     {
         "id": 2,
-        "from": "Istanbul(SAW)",
-        "to": "Ankara(ESB)",
-        "departure": "2021-10-10",
-        "passenger": 1,
-        "price": 100
+        "airline": "Pegasus Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "09:00",
+        "remainingSeat": 3,
+        "price": "100"
     },
     {
         "id": 3,
-        "from": "Ankara(ESB)",
+        "airline": "Turkish Airlines",
+        "from": "Antalya(AYT)",
         "to": "Istanbul(SAW)",
-        "departure": "2021-10-10",
-        "passenger": 1,
-        "price": 100
+        "departure": "2023-10-02",
+        "departureTime": "11:00",
+        "remainingSeat": 4,
+        "price": "300"
     },
+    {
+        "id": 4,
+        "airline": "Turkish Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "10:30",
+        "remainingSeat": 1,
+        "price": "250"
+    },
+    {
+        "id": 5,
+        "airline": "Pegasus Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "09:30",
+        "remainingSeat": 2,
+        "price": "150"
+    },
+    {
+        "id": 6,
+        "airline": "Turkish Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "11:30",
+        "remainingSeat": 5,
+        "price": "350"
+    },
+    {
+        "id": 7,
+        "airline": "AnadoluJet",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "10:00",
+        "remainingSeat": 1,
+        "price": "300"
+    },
+    {
+        "id": 8,
+        "airline": "Pegasus Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "12:00",
+        "remainingSeat": 2,
+        "price": "200"
+    },
+    {
+        "id": 9,
+        "airline": "Turkish Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "13:00",
+        "remainingSeat": 3,
+        "price": "400"
+    },
+    {
+        "id": 10,
+        "airline": "Pegasus Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "14:00",
+        "remainingSeat": 4,
+        "price": "250"
+    },
+    {
+        "id": 11,
+        "airline": "Turkish Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "12:30",
+        "remainingSeat": 5,
+        "price": "300"
+    },
+    {
+        "id": 12,
+        "airline": "Pegasus Airlines",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "11:30",
+        "remainingSeat": 6,
+        "price": "350"
+    },
+    {
+        "id": 13,
+        "airline": "AnaodoluJet",
+        "from": "Antalya(AYT)",
+        "to": "Istanbul(SAW)",
+        "departure": "2023-10-02",
+        "departureTime": "11:30",
+        "remainingSeat": 7,
+        "price": "400"
+    },
+    {
+        "id": 14,
+        "airline": "Turkish Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "16:00",
+        "remainingSeat": 7,
+        "price": "200"
+    },
+    {
+        "id": 15,
+        "airline": "Pegasus Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "16:30",
+        "remainingSeat": 3,
+        "price": "100"
+    },
+    {
+        "id": 16,
+        "airline": "Turkish Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "17:00",
+        "remainingSeat": 4,
+        "price": "300"
+    },
+    {
+        "id": 17,
+        "airline": "Turkish Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "17:30",
+        "remainingSeat": 1,
+        "price": "250"
+    },
+    {
+        "id": 18,
+        "airline": "Pegasus Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "19:30",
+        "remainingSeat": 2,
+        "price": "150"
+    },
+    {
+        "id": 19,
+        "airline": "Turkish Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "20:30",
+        "remainingSeat": 5,
+        "price": "350"
+    },
+    {
+        "id": 20,
+        "airline": "AnadoluJet",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "19:00",
+        "remainingSeat": 1,
+        "price": "300"
+    },
+    {
+        "id": 21,
+        "airline": "Pegasus Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "20:00",
+        "remainingSeat": 2,
+        "price": "200"
+    },
+    {
+        "id": 22,
+        "airline": "Turkish Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "21:00",
+        "remainingSeat": 3,
+        "price": "400"
+    },
+    {
+        "id": 23,
+        "airline": "Pegasus Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "22:00",
+        "remainingSeat": 4,
+        "price": "250"
+    },
+    {
+        "id": 24,
+        "airline": "Turkish Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "21:30",
+        "remainingSeat": 5,
+        "price": "300"
+    },
+    {
+        "id": 25,
+        "airline": "Pegasus Airlines",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "22:30",
+        "remainingSeat": 6,
+        "price": "350"
+    },
+    {
+        "id": 26,
+        "airline": "AnaodoluJet",
+        "from": "Istanbul(SAW)",
+        "to": "Antalya(AYT)",
+        "departure": "2023-10-03",
+        "departureTime": "23:30",
+        "remainingSeat": 7,
+        "price": "400"
+    }
 ]
 
 export const from = [
