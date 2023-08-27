e5a3d6bcab01a9a571c4a079a301afcd8dada8cc
Alperen ERYILMAZ <mchtter@gmail.com>
2023-08-27 14:20:06 +0300

    client[develop]: loading spinner component added

---
+++ src/components/Loading.js
@@ -0,0 +1,7 @@
+export function Loading() {
+    return (
+        <div className="flex items-center justify-center h-screen">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-950 dark:border-gray-100 z-40"></div>
+        </div>
+    )
+}
